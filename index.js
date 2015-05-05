9080f0dcc3bb176493f00c1885486ad293b9ab7a
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-05-05 21:54:33 +0000

    accept both strings and objects as spec arg

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Types = require('./lib/types.js');
 var Converter = module.exports = {};
 
 Converter.convert = function(options, callback) {
-  var fromSpec = options.url ? new Types[options.from](options.url) : Types.parse(options.spec);
+  var fromSpec = options.url ? new Types[options.from](options.url) : Types.build(options.spec, options.from);
   fromSpec.maybeResolveFromUrl(function() {
     var toSpec = fromSpec.convertTo(options.to);
     callback(null, toSpec);
