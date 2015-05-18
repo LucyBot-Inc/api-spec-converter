165660cad34d19d93a23dc175cb54f034cb7f2e3
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-05-18 17:43:25 +0000

    Some refactoring - type is in charge of resolving files and URLs, base type handles default case

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 var FS = require('fs');
 var Request = require('request');
-var Types = require('./lib/types.js');
+var Types = require('./lib/types/types.js');
 
 var Converter = module.exports = {};
 
 Converter.convert = function(options, callback) {
-  var makeSpec = function() {
-    var fromSpec = Types.build(options.spec, options.from);
-    fromSpec.resolveResources(options, function() {
-      var toSpec = fromSpec.convertTo(options.to);
-      callback(null, toSpec);
-    });
-  }
-  if (options.url) {
-    Request(options.url, function(err, resp, body) {
-      if (err) return callback(err);
-      options.spec = body;
-      makeSpec();
-    });
-  } else if (options.file) {
-    FS.readFile(options.file, 'utf8', function(err, body) {
-      options.spec = body;
-      makeSpec();
-    })
-  } else {
-    makeSpec();
-  }
+  var fromSpec = new Types[options.from]();
+  fromSpec.resolveResources(options, function() {
+    var toSpec = fromSpec.convertTo(options.to);
+    callback(null, toSpec);
+  });
 }
