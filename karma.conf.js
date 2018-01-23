748a4ed4432e2c4ce3e0f5600891403ea61eedbf
Travis CI <bobby.brennan@gmail.com>
2018-01-23 20:49:24 +0000

    remove Sauce

--- karma.conf.js
+++ karma.conf.js
@@ -1,28 +1,5 @@
 module.exports = function (config) {
-    var customLaunchers = {
-      sl_chrome: {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        platform: 'Windows 7',
-        version: '35'
-      },
-      sl_firefox: {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        version: '30'
-      },
-      sl_ie_11: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8.1',
-        version: '11'
-      }
-    };
-
     var browsers = ['PhantomJS'];
-    if (process.env.SAUCE === "true") {
-      browsers = browsers.concat(Object.keys(customLaunchers));
-    }
 
     config.set({
         frameworks: ['mocha', 'chai'],
@@ -41,8 +18,6 @@ module.exports = function (config) {
         },
 
         reporters: ['mocha'],
-
-        customLaunchers: customLaunchers,
 
         browsers: browsers,
 
